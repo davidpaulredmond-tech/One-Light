334c3311eebcc62f70284152ced7ea1799fdd9f1
davidpaulredmond-tech <davidpaulredmond@gmail.com>
2026-04-03 01:02:31 -0400

    Fix audio file name case sensitivity

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ let sessionInterval = null;
 let secondsRemaining = SESSION_SECONDS;
 let oceanPlaying = false;
 
-const oceanAudio = new Audio("ocean-v2.mp3?v=999");
+const oceanAudio = new Audio("Ocean-v2.mp3?v=999");
 oceanAudio.loop = true;
 oceanAudio.volume = 0.9;
 
