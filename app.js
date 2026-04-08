1f0934877a7545479205de41d3e8a4d95b6b848d
davidpaulredmond-tech <davidpaulredmond@gmail.com>
2026-04-07 21:52:28 -0400

    Update ocean audio file version to v4

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ let sessionInterval = null;
 let secondsRemaining = SESSION_SECONDS;
 let oceanPlaying = false;
 
-const oceanAudio = new Audio("ocean-v3.mp3?v=3");
+const oceanAudio = new Audio("ocean-v3.mp3?v=4");
 oceanAudio.loop = true;
 oceanAudio.volume = 0.9;
 
